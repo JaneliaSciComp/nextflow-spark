5278e5e0d37f698212f5f0e3214ca7d84ed69682
Cristian Goina <goinac@janelia.hhmi.org>
2020-12-11 16:37:38 -0500

    example for running with singularity

---
+++ examples/example-with-localsingularity.sh
@@ -0,0 +1,15 @@
+./main.nf -profile localsingularity \
+	--runtime_opts "-B $PWD/local:$PWD/local" \
+	--workers 1 \
+	--spark_log_dir $PWD/local \
+	--app_jar $PWD/local/app.jar \
+	--app_main org.janelia.colormipsearch.cmd.SparkMainEntry \
+	--app_args \
+	"searchLocalFiles, \
+	-m,$PWD/local/testData/masks/ch2ch_2_mask.png, \
+	-i,$PWD/local/testData/flyem/766255970_RT_18U_FL-02_CDM.tif, \
+    --mirrorMask, \
+    --xyShift,2, \
+    --pctPositivePixels,2, \
+    --pixColorFluctuation,1, \
+    -od $PWD/local/testData/cdsresults.test"
