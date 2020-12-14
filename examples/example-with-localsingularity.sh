12e220e258cc1c491690965b354af58caf6a7dc2
Cristian Goina <goinac@janelia.hhmi.org>
2020-12-14 12:17:59 -0500

    changed singularity example

--- examples/example-with-localsingularity.sh
+++ examples/example-with-localsingularity.sh
@@ -1,15 +1,21 @@
 ./main.nf -profile localsingularity \
 	--runtime_opts "-B $PWD/local:$PWD/local" \
-	--workers 1 \
+	--workers 2 \
+	--worker_cores 2 \
+	--gb_per_core 4 \
+	--driver_cores 1 \
+	--driver_memory 2g \
 	--spark_work_dir "$PWD/local" \
 	--app_jar "$PWD/local/app.jar" \
 	--app_main org.janelia.colormipsearch.cmd.SparkMainEntry \
 	--app_args \
-	"searchLocalFiles, \
-	-m,$PWD/local/testData/masks/ch2ch_2_mask.png, \
-	-i,$PWD/local/testData/flyem/766255970_RT_18U_FL-02_CDM.tif, \
-    --mirrorMask, \
-    --xyShift,2, \
-    --pctPositivePixels,2, \
-    --pixColorFluctuation,1, \
-    -od $PWD/local/testData/cdsresults.test"
+	"searchFromJSON, \
+	-m,$PWD/local/testData/mask.json, \
+	-i,$PWD/local/testData/mcfoTargets.json, \
+        --mirrorMask, \
+        --maskThreshold,100, \
+        --dataThreshold,100, \
+        --xyShift,0, \
+        --pctPositivePixels,2, \
+        --pixColorFluctuation,2, \
+        -od $PWD/local/testData/cdsresults.test"
