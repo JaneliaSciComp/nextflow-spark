03ff8730673feaf04ad598522ff06cef522d3c32
Cristian Goina <goinac@janelia.hhmi.org>
2020-12-16 19:38:19 -0500

    so it works with 1 worker job and 3 more cores for job then executor cores

--- examples/example-with-lsf-1.sh
+++ examples/example-with-lsf-1.sh
@@ -1,18 +1,21 @@
 ./main.nf -profile lsf \
-	--runtime_opts "-B $PWD/local -B /nrs/jacs/jacsData/filestore" \
-	--workers 3 \
-	--worker_cores 3 \
+	--runtime_opts "-e -B $PWD/local -B $PWD/examples -B /nrs/jacs/jacsData/filestore" \
+        --lsf_opts "-P scicompsoft -q test" \
+	--workers 1 \
+	--worker_cores 15 \
+        --executor_cores 12 \
 	--gb_per_core 15 \
 	--driver_cores 1 \
 	--driver_memory 10g \
+        --driver_deploy_mode client \
 	--driver_logconfig $PWD/examples/spark-conf/log4j.properties \
 	--spark_work_dir "$PWD/local" \
-	--app "$PWD/local/app.jar" \
+	--app $PWD/local/app.jar \
 	--app_main org.janelia.colormipsearch.cmd.SparkMainEntry \
 	--app_args \
 	"searchFromJSON, \
 	-m $PWD/local/testData/mask-2.json, \
-	-i $PWD/local/testData/mcfoTargets.json, \
+	-i $PWD/local/testData/sgal4Targets.json, \
 	--mirrorMask, \
 	--maskThreshold 100, \
 	--dataThreshold 100, \
